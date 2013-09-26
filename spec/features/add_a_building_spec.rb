9f1a75f4c43237f5e0774c93a760f2b54b0bdf80
Doug Gerber <dojoh@outlook.com>
2013-09-26 13:11:07 -0400

    Add Tests and Migration

--- spec/features/add_a_building_spec.rb
+++ spec/features/add_a_building_spec.rb
@@ -15,7 +15,18 @@ feature 'add a building', %Q{
 # If I do not specify all of the required information in the required formats, the building is not recorded and I am presented with errors
 # Upon successfully creating a building, I am redirected so that I can record another building.
 
-	scenario 'records a building entry' do
+	scenario 'records a valid building entry' do
+		prev_count = Building.count
 
+		visit new_building_url
+		fill_in 'Street Address', with: '5 Apple Street'
+		fill_in 'City', with: 'Boston'
+		fill_in 'State', with: 'Massachusetts'
+		fill_in 'Postal Code', with: 02215
+
+		click_button 'Submit'
+		expect(page).to have_content("You've entered a record successfully")
+		expect(Building.count).to eql(prev_count + 1)
 	end
+	scenario 'does not record an invalid entry'
 end
---
+++ spec/models/building_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Building do
+	it { should have_valid(:street_address).when('5 Apple Street', '5 Apple St.') }
+	it { should_not have_valid(:street_address).when(nil, '') }
+
+	it { should have_valid(:city).when('Boston') }
+	it { should_not have_valid(:city).when(nil, '') }
+
+	it { should have_valid(:state).when('Massachusetts') }
+	it { should_not have_valid(:state).when(nil, '') }
+
+	it { should have_valid(:postal_code).when(02215) }
+	it { should_not have_valid(:postal_code).when(nil, '') }
+end
