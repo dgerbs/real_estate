c06062aa41d21c0ff0246db88f2bdd5664cc67fa
Doug Gerber <dojoh@outlook.com>
2013-09-26 15:11:29 -0400

    Association Model Owner and Company Unit Tests Pass

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,4 +2,7 @@ class Owner < ActiveRecord::Base
 	validates_presence_of :first_name
 	validates_presence_of :last_name
 	validates_presence_of :email_address
+
+	belongs_to :company,
+		inverse_of: :owners
 end
